fe61139fdc87751d15cd6cceca0690984af08049
Djavu815 <camilogallego0815@gmail.com>
2025-12-04 20:50:33 -0500

    Tf state

--- main.tf
+++ main.tf
@@ -20,3 +20,20 @@ resource "aws_s3_bucket" "milo-gha-tf" {
     bootcamp     = "devops"
   }
 }
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "244190102671-camilo-gallego"
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
