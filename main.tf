eda8ee0fdbf4ae03f0932936bb9d1e0b1800cfe0
Djavu815 <camilogallego0815@gmail.com>
2025-12-04 19:40:31 -0500

    Terraform gha using ubuntu-22.04

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.52.0"
+    }
+    random = {
+      version = "~> 3.6.2"
+    }
+  }
+
+  required_version = "~> 1.13.4"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "milo-gha-tf" {
+  bucket = "244190102671-camilo-gallego"
+}
